aa00f818d2bd4a5579dbddc805efddaf9354c651
krrish <krishkadam88@gmail.com>
2025-04-29 16:42:44 +0530

    update booking model

--- src/components/shared/booking-model.tsx
+++ src/components/shared/booking-model.tsx
@@ -40,7 +40,7 @@ const BookingModel = ({
         <DialogHeader>
           <DialogTitle>Update Booking</DialogTitle>
           <DialogDescription>
-            Make changes to booking here. Click save when you're done.
+            Make changes to booking here. Click save when you&apos;re done.
           </DialogDescription>
         </DialogHeader>
         <ScrollArea className="w-full h-[calc(100vh-200px)]">
